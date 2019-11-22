2acec75961711c592b2668f7fea56e354b919bac
maheshrnabhat <58079942+maheshrnabhat@users.noreply.github.com>
2019-11-22 06:54:03 -0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostram>
+#include <string>
+using namespace std;
+
+int main()
+ string txt;
+ cout << "what is your name and surname ?" <<end;
+ getline (cin,Txt);
+ cout << "your name is :" <<Txt,substr (0,Txt. find ()
+
+ cout << Txt.lenghth () <<end; //length of text
+ cout <<Txt.find("e") << endl; //position of e
+ cout <<Txt.substr ()
+
+
+
+}
+
+
